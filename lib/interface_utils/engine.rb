3822a6412f519ef0d4e853db87d0f43bf7d8152d
Andres Montano <amontano@virginia.edu>
2013-05-31 00:14:54 -0600

    adding tiny mce styling and images

--- lib/interface_utils/engine.rb
+++ lib/interface_utils/engine.rb
@@ -1,4 +1,7 @@
 module InterfaceUtils
   class Engine < ::Rails::Engine
+    initializer :assets do |config|
+      Rails.application.config.assets.precompile.concat(['interface_utils/customtinymce.css'])
+    end
   end
 end
