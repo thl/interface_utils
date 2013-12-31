ee32201d0cceca27523c64aadc8d1ca0bbd4ac0a
Andres Montano <amontano@virginia.edu>
2013-12-30 18:50:29 -0600

    fixing shared cache for app settings

--- lib/interface_utils/application_settings.rb
+++ lib/interface_utils/application_settings.rb
@@ -3,7 +3,7 @@ module InterfaceUtils
     private
 
     def self.settings
-      settings = Rails.cache.fetch('application_settings/hash', :expires_in => 1.day) do
+      settings = Rails.cache.fetch("application_settings/#{InterfaceUtils::Server.get_domain}/hash", :expires_in => 1.day) do
         settings_file = Rails.root.join('config', 'settings.yml')
         settings_file.exist? ? YAML.load_file(settings_file) : {}
       end
