f4f4b60c9dadd4bc439fec39961571d72df6d940
Andres Montano <amontano@virginia.edu>
2014-09-19 09:57:29 -0600

    adding setting to enable or disable connecting associated resources to localhost

--- lib/interface_utils/server.rb
+++ lib/interface_utils/server.rb
@@ -8,19 +8,28 @@ module InterfaceUtils
     APOC        = 6
     EBHUTAN     = 7
     
+    def self.enable_localhost?
+      enable_localhost = Rails.cache.fetch("application_settings/#{InterfaceUtils::Server.get_domain}/enable_localhost", :expires_in => 1.day) do
+        str = InterfaceUtils::ApplicationSettings.settings['enable.localhost']
+        break false if str.blank?
+        str.downcase!
+        str.strip!
+        str=='yes'
+      end
+    end
+    
     def self.environment
       Rails.cache.fetch('server/environment', :expires_in => 1.day) do
         hostname = Socket.gethostname.downcase
         case hostname
         when 'sds6.itc.virginia.edu'     then STAGING
         when 'dev.thlib.org'             then DEVELOPMENT
-        when 'dev.thlib.org'             then DEVELOPMENT
         when 'apoc.village.virginia.edu' then APOC
         when 'e-bhutan.bt'               then EBHUTAN
         else
           if hostname =~ /sds.+\.it[c|s]\.virginia\.edu/
             PRODUCTION
-          elsif hostname.ends_with?('local') || hostname.starts_with?('vpn-user')
+          elsif enable_localhost? && (hostname.ends_with?('local') || hostname.starts_with?('vpn-user'))
             LOCAL
           else
             OTHER
