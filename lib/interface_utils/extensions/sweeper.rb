eda0b7a77730c70c80391dc7866e0491edb2305f
Andres Montano <amontano@virginia.edu>
2015-04-07 23:33:36 -0600

    making sweeper correction code backward compatible

---
+++ lib/interface_utils/extensions/sweeper.rb
@@ -0,0 +1,25 @@
+module InterfaceUtils
+  module Extensions
+    module Sweeper
+      extend ActiveSupport::Concern
+  
+      def cache_configured?
+        Rails.configuration.action_controller.cache_configured?
+      end
+
+      included do
+      end
+  
+      module ClassMethods
+        # Very weird! ActionController::Caching seems to assume it is being called from controller. Adding this as hack
+        def perform_caching
+          Rails.configuration.action_controller.perform_caching
+        end
+        
+        def default_static_extension
+          Rails.configuration.action_controller.default_static_extension
+        end
+      end
+    end
+  end
+end
