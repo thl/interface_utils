373cbf7875bf537605ff441601d2bf700fb29755
Andres Montano <amontano@virginia.edu>
2025-09-19 20:17:53 -0600

    using sweeper concerns

---
+++ app/sweepers/concerns/interface_utils/sweeper_extensions.rb
@@ -0,0 +1,28 @@
+module InterfaceUtils
+  module SweeperExtensions
+    extend ActiveSupport::Concern
+    
+    included do
+      @@page_cache_directory = "public"
+    end
+    
+    def cache_configured?
+      Rails.configuration.action_controller.cache_configured?
+    end
+    
+    def expire_full_path_page(path)
+      expire_page("#{@@page_cache_directory}#{path}")
+    end
+    
+    def default_static_extension
+      ActionController::Base.default_static_extension
+    end
+
+    module ClassMethods
+      # Very weird! ActionController::Caching seems to assume it is being called from controller. Adding this as hack
+      def perform_caching
+        Rails.configuration.action_controller.perform_caching
+      end
+    end
+  end
+end
--- lib/interface_utils.rb
+++ lib/interface_utils.rb
@@ -1,7 +1,6 @@
 require 'interface_utils/engine'
 require 'interface_utils/server'
 require 'interface_utils/application_settings'
-require 'interface_utils/extensions/sweeper'
 
 module InterfaceUtils
 end
