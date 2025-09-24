b24b1a2c6bceb0fb8bb116d3cf8a780155e9918d
Andres Montano <amontano@virginia.edu>
2025-09-24 12:08:27 -0600

    updating code

--- app/sweepers/concerns/interface_utils/sweeper_extensions.rb
+++ app/sweepers/concerns/interface_utils/sweeper_extensions.rb
@@ -18,7 +18,7 @@ module InterfaceUtils
       ActionController::Base.default_static_extension
     end
 
-    module ClassMethods
+    class_methods do
       # Very weird! ActionController::Caching seems to assume it is being called from controller. Adding this as hack
       def perform_caching
         Rails.configuration.action_controller.perform_caching
