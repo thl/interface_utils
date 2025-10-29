19ee716f5fe591418ee461a361942b44825ea0a1
Andres Montano <amontano@virginia.edu>
2025-10-28 20:30:57 -0600

    adding human cookie

--- app/assets/javascripts/interface_utils/application.js
+++ app/assets/javascripts/interface_utils/application.js
@@ -10,5 +10,8 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+//= require jquery
+//= require jquery_ujs
+//= require tinymce-jquery
 //= require interface_utils/jquery-ui-1.10.4.custom.min
 //= require interface_utils/human
